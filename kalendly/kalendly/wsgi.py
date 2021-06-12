6b3c1520cf9463bd215fa0d9c0068304984adf4f
Peeyush <peeyush.raj@gmail.com>
2021-06-12 11:53:45 +0530

    Black Formatter

--- kalendly/kalendly/wsgi.py
+++ kalendly/kalendly/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kalendly.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kalendly.settings")
 
 application = get_wsgi_application()
