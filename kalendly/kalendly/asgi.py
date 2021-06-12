fabe65f0d649afd224256f92a47d8854c611519c
Peeyush <peeyush.raj@gmail.com>
2021-06-12 11:47:55 +0530

    rename the project

--- kalendly/kalendly/asgi.py
+++ kalendly/kalendly/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for kalendely project.
+ASGI config for kalendly project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kalendely.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kalendly.settings')
 
 application = get_asgi_application()
